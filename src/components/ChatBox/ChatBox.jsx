e15b572eb53b737d659ca8b041ec1c6895d8e6f3
Gaurav-Attri <attrigaurav60@gmail.com>
2025-08-23 11:05:07 +0530

    Chat's Left sidebar UI done.

---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ChatBox'
+
+const ChatBox = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatBox
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Chat.css'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>
-      
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSidebar />
+        <ChatBox />
+        <RightSidebar />
+      </div>
     </div>
   )
 }
